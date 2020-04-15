43262ba54e1ff75bec3245d8254d2082e051a784
bianchunhui <chunhui.bian@gmail.com>
2020-04-15 14:00:43 +0800

    idea

---
+++ src/main/java/com/example/demo/controller/hellocontroller.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@EnableAutoConfiguration
+
+public class hellocontroller {
+    @RequestMapping("/helloworld")
+    public String hello() {
+     return ("hello");
+    }
+}
